a958240524345aee48bc539caad7a4d01e12e713
Lisa Ridley <lisa@codementality.com>
2019-09-06 15:03:29 -0400

    Expanding Functional Tests

---
+++ docroot/modules/custom/team_work/tests/src/Functional/TeamWorkVocabulariesTestCase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\Tests\team_work\Functional;
+
+use Drupal\taxonomy\Entity\Vocabulary;
+/**
+ * Test that proper vocabularies exist.
+ *
+ * @group team_work
+ */
+class TeamWorkVocabulariesTestCase extends TeamWorkFunctionalTestBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    // Make sure to complete the normal setup steps first.
+    parent::setUp();
+
+    // Get list of vocabularies on the site.
+
+  }
+
+
+  /**
+   * Make sure everything works to this point.
+   */
+  public function testVocabulariesArePresent() {
+  	$vocabularies = Vocabulary::loadMultiple();
+
+  	// Checks that there are 7 vocabularies.
+    $this->assertCount(7, $vocabularies);
+
+    // Checks for the existence of the correct vocabularies.
+    $vocabulary_ids = array_keys($vocabularies);
+
+    $this->assertTrue(in_array('issue_priority', $vocabulary_ids));
+    $this->assertTrue(in_array('issue_status', $vocabulary_ids));
+    $this->assertTrue(in_array('issue_type', $vocabulary_ids));
+    $this->assertTrue(in_array('linked_issue_status', $vocabulary_ids));
+    $this->assertTrue(in_array('project_keys', $vocabulary_ids));
+    $this->assertTrue(in_array('sprint', $vocabulary_ids));
+    $this->assertTrue(in_array('team_roles', $vocabulary_ids));
+  }
+
+}
